2f965cab3542ad65eaa911189602e77a40ea3777
ThemySabri <themysabriii@gmail.com>
2024-07-07 15:29:22 +0700

    mod: precess_data.py

--- process_data.py
+++ process_data.py
@@ -34,24 +34,23 @@ rata2_sampah_tahunan.to_csv('rata2_sampah_tahunan.csv', index=False)
 provinsi_terbanyak.to_csv('provinsi_terbanyak.csv', index=False)
 provinsi_tersedikit.to_csv('provinsi_tersedikit.csv', index=False)
 
-# Membuat dan menyimpan grafik jumlah total sampah tahunan di setiap provinsi dari tahun ke tahun
+# Membuat dan menyimpan grafik jumlah total sampah tahunan di setiap provinsi untuk tahun 2023 dalam bentuk diagram batang
 plt.figure(figsize=(10, 6))
-for provinsi in total_sampah_tahunan['Provinsi'].unique():
-    df_provinsi = total_sampah_tahunan[total_sampah_tahunan['Provinsi'] == provinsi]
-    plt.plot(df_provinsi['Tahun'],
-             df_provinsi['Timbulan Sampah Tahunan(ton)'], label=provinsi)
-
-plt.xlabel('Tahun')
+total_sampah_2023 = total_sampah_tahunan[total_sampah_tahunan['Tahun'] == 2023]
+plt.bar(total_sampah_2023['Provinsi'],
+        total_sampah_2023['Timbulan Sampah Tahunan(ton)'], color='blue')
+plt.xlabel('Provinsi')
 plt.ylabel('Timbulan Sampah (ton)')
-plt.title('Jumlah Total Sampah Tahunan di Setiap Provinsi')
-plt.legend()
-plt.savefig('static/total_sampah_tahunan.png')
+plt.title('Jumlah Total Sampah Tahunan di Setiap Provinsi (2023)')
+plt.savefig('static/total_sampah_tahunan_2023.png')
 plt.close()
 
 # Membuat dan menyimpan grafik rata-rata timbulan sampah tahunan di setiap provinsi
 plt.figure(figsize=(10, 6))
-plt.bar(rata2_sampah_tahunan['Provinsi'], rata2_sampah_tahunan['Timbulan Sampah Tahunan(ton)'], color=[
-        'green' if x <= 100000 else 'orange' if x <= 700000 else 'red' for x in rata2_sampah_tahunan['Timbulan Sampah Tahunan(ton)']])
+colors = ['green' if x <= 100000 else 'orange' if x <=
+          700000 else 'red' for x in rata2_sampah_tahunan['Timbulan Sampah Tahunan(ton)']]
+plt.bar(rata2_sampah_tahunan['Provinsi'],
+        rata2_sampah_tahunan['Timbulan Sampah Tahunan(ton)'], color=colors)
 plt.xlabel('Provinsi')
 plt.ylabel('Rata-rata Timbulan Sampah (ton)')
 plt.title('Rata-rata Timbulan Sampah Tahunan di Setiap Provinsi')
